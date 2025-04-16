3d9898355c8f39d94756408cb86b6f946da9d956
6oncvlo <goncaloarv@gmail.com>
2025-04-16 22:28:13 +0100

    fastapi

--- ml-api/app/app.py
+++ ml-api/app/app.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from .inference import predict
 
 app = FastAPI()
 
+# CORS is needed here!
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# input schema
 class InputData(BaseModel):
     feature1: float
     feature2: float
--- ml-api/app/inference.py
+++ ml-api/app/inference.py
@@ -1,15 +1,10 @@
-import sys
-import os
-import numpy as np
 import joblib
-
-sys.path.append(os.path.abspath('..'))
 
 # load model
 model = joblib.load('app/artifacts/model.joblib')
 
-def predict(feature1: float, feature2: float):
-
-    features = np.array([[feature1, feature2]])
-    prediction = model.predict(features)[0].astype(int)
-    return prediction
+# predict function
+def predict(feature1: float, feature2: float) -> int:
+    input_features = [[feature1, feature2]]
+    prediction = model.predict(input_features)[0]
+    return int(prediction)
