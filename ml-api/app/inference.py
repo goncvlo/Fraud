de4d0e1fd0a81b0b0dd518cb58e0a6a9831568fd
6oncvlo <goncaloarv@gmail.com>
2025-04-17 23:43:28 +0100

    path

--- ml-api/app/inference.py
+++ ml-api/app/inference.py
@@ -1,7 +1,7 @@
 import joblib
 
 # load model
-model = joblib.load('app/artifacts/model.joblib')
+model = joblib.load('ml-api/app/artifacts/model.joblib')
 
 # predict function
 def predict(feature1: float, feature2: float) -> int:
