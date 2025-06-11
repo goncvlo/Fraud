763622fa474f3c6e44f746650cd6a5d6c61308c8
6oncvlo <goncaloarv@gmail.com>
2025-06-11 19:18:29 +0100

    z-score

--- src/data/prepare_data.py
+++ src/data/prepare_data.py
@@ -4,5 +4,7 @@ def prepare_data(df: pd.DataFrame):
 
     df = df.rename(columns={'Class': 'label'})
     df.columns = [col.lower() for col in df.columns]
+    for col in ['time', 'amount']:
+        df[col] = (df[col]-df[col].mean())/df[col].std()
 
     return df
