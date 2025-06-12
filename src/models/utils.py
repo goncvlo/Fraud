5b423866a68e72787375d2cec0773bb2079e59c8
6oncvlo <goncaloarv@gmail.com>
2025-06-12 16:50:19 +0100

    reproducibility and NeuralNetworkClassifier params

--- src/models/utils.py
+++ src/models/utils.py
@@ -3,17 +3,23 @@ from sklearn.model_selection import train_test_split
 from imblearn.under_sampling import RandomUnderSampler
 from imblearn.over_sampling import RandomOverSampler
 
+import os
+import numpy as np
+import random
+import tensorflow as tf
+
+
 def train_splits(features: pd.DataFrame, labels: pd.Series, config: dict):
     """Split train set into multiple sets using stratified sampling."""
 
     train_sets = {}
     train_sets[0] = pd.concat([features, labels], axis=1)
 
-    for i in range(len(config['train_sizes'])):
+    for i in range(len(config["train_sizes"])):
         
         X_train_1, X_train_2, y_train_1, y_train_2 = train_test_split(
             train_sets[0].iloc[:,:-1], train_sets[0].iloc[:,-1]
-            , test_size=config['train_sizes'][i]
+            , test_size=config["train_sizes"][i]
             , random_state=123
             , shuffle=True
             , stratify=train_sets[0].iloc[:,-1]
@@ -27,17 +33,18 @@ def train_splits(features: pd.DataFrame, labels: pd.Series, config: dict):
 
     return train_sets
 
+
 def imbalanced_sampling(method: str, X_train: pd.DataFrame, y_train: pd.Series, **kwargs):
     """Apply over or under sampling."""
 
-    if method=='over':
+    if method=="over":
         sampler = RandomOverSampler(
-            sampling_strategy='minority',
+            sampling_strategy="minority",
             random_state=42
         )
-    elif method=='under':
+    elif method=="under":
         sampler = RandomUnderSampler(
-            sampling_strategy='majority'
+            sampling_strategy="majority"
             , random_state=42
             )
     else:
@@ -47,3 +54,15 @@ def imbalanced_sampling(method: str, X_train: pd.DataFrame, y_train: pd.Series, 
     X_train_rs, y_train_rs = sampler.fit_resample(X_train, y_train)
 
     return X_train_rs, y_train_rs
+
+
+def set_global_seed(seed: int = 42):
+    #os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    tf.random.set_seed(seed)
+
+    # force tensorflow to be deterministic
+    #os.environ["TF_DETERMINISTIC_OPS"] = "1"
+    tf.config.experimental.enable_op_determinism()
+    
