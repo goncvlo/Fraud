8dd02f097d0ed163173bc7d5c844bb59408b3260
6oncvlo <goncaloarv@gmail.com>
2025-08-30 18:03:34 +0100

    roc_auc

--- src/models/tuner.py
+++ src/models/tuner.py
@@ -6,7 +6,7 @@ from sklearn.base import clone
 import optuna
 
 from src.models.model import Classifier
-from src.models.evaluation import Evaluation
+from src.models.evaluator import Evaluation
 
 
 class HyperParamSearch:
@@ -20,7 +20,7 @@ class HyperParamSearch:
 
     def fit(
             self
-            , X: pd.DataFrame, y: pd.Series # training set
+            , X_train: pd.DataFrame, y_train: pd.Series # training set
             , trial: optuna.trial.Trial
             , X_val: pd.DataFrame = None, y_val: pd.Series = None
             ) -> float:
@@ -31,17 +31,17 @@ class HyperParamSearch:
 
         # holdout validation for algorithm comparasion
         if X_val is not None and y_val is not None:
-            clf.fit(X, y)
+            clf.fit(X_train, y_train)
             scorer = Evaluation(clf=clf, threshold=0.5)
-            score = scorer.fit(train=(X, y), validation=(X_val, y_val))
+            score = scorer.fit(train=(X_train, y_train), test=(X_val, y_val))
             self.results[trial.number] = score
-            return score.loc["validation", self.scoring_metric]
+            return score.loc["test", self.scoring_metric]
 
         # strat kfold cv on training data
         else:
             cv_scores = cross_val_score(
                 estimator=clf.model,
-                X=X, y=y,
+                X=X_train, y=y_train,
                 scoring=self.scoring_metric,
                 cv=self.cross_validator
                 )
@@ -87,7 +87,7 @@ class LabelWeightSearch:
         self.model = estimator.model
         self.scoring_metric = config["scoring_metric"]
 
-    def fit(self, X: pd.DataFrame, y: pd.Series, trial: optuna.trial.Trial):
+    def fit(self, X_train: pd.DataFrame, y_train: pd.Series, trial: optuna.trial.Trial):
 
         w0 = trial.suggest_int("weight_0", 1.0, 100.0)
         w1 = trial.suggest_int("weight_1", 1.0, 100.0)
@@ -95,15 +95,15 @@ class LabelWeightSearch:
         skf = StratifiedKFold(n_splits=self.cross_validator, shuffle=True, random_state=42)
         scores = []
 
-        for fold, (train_idx, val_idx) in enumerate(skf.split(X, y)):
-            X_train, X_val = X.iloc[train_idx], X.iloc[val_idx]
-            y_train, y_val = y.iloc[train_idx], y.iloc[val_idx]
+        for fold, (train_idx, val_idx) in enumerate(skf.split(X_train, y_train)):
+            X_train_, X_val = X_train.iloc[train_idx], X_train.iloc[val_idx]
+            y_train_, y_val = y_train.iloc[train_idx], y_train.iloc[val_idx]
 
             sample_weight = compute_sample_weight(
-                class_weight={0: w0, 1: w1}, y=y_train
+                class_weight={0: w0, 1: w1}, y=y_train_
                 )
             model = clone(self.model) # avoid retaining internal state after fitted > fresh estimator
-            model.fit(X_train, y_train, sample_weight=sample_weight)
+            model.fit(X_train_, y_train_, sample_weight=sample_weight)
 
             score = Evaluation(clf=model)
             score = score.fit(metric=self.scoring_metric, test=(X_val, y_val))
@@ -118,7 +118,7 @@ class ClassifierThreshold():
         self.cross_validator = config["cross_validator"]
         self.scoring_metric = config["scoring_metric"]
     
-    def fit(self, clf: Classifier, X: pd.DataFrame, y: pd.Series):
+    def fit(self, clf: Classifier, X_train: pd.DataFrame, y_train: pd.Series):
         """Fits TunedThresholdClassifierCV to get best threshold."""
 
         tuned_clf = TunedThresholdClassifierCV(
@@ -130,7 +130,7 @@ class ClassifierThreshold():
             , random_state=42
             , store_cv_results=True
         )
-        tuned_clf.fit(X=X, y=y)
+        tuned_clf.fit(X=X_train, y=y_train)
         
         self.model = tuned_clf
         self.best_threshold = tuned_clf.best_threshold_
