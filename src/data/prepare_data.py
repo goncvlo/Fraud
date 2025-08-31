2de8ca1c763bea4d966a8afb2df9c1190efea94b
6oncvlo <goncaloarv@gmail.com>
2025-08-31 16:02:56 +0100

    label as float

--- src/data/prepare_data.py
+++ src/data/prepare_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 def prepare_data(df: pd.DataFrame):
 
     df = df.rename(columns={'Class': 'label'})
+    df["label"] = df["label"].astype("float")
     df.columns = [col.lower() for col in df.columns]
     for col in ['time', 'amount']:
         df[col] = (df[col]-df[col].mean())/df[col].std()
